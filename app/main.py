6a083830a23f9edc9fa6f69bbf4d644f35429a46
Priyatham10 <kunarapupriyatham@gmail.com>
2024-09-12 03:15:58 +0530

    Storing all the tokens using Dict and a Scanner class for lexer implementation is created

--- app/main.py
+++ app/main.py
@@ -1,44 +1,37 @@
 import sys
 
+all_tkns = {
+    '(' : "LEFT_PAREN",
+    ')' : "RIGHT_PAREN",
+    '{' : "LEFT_BRACE",
+    '}' : "RIGHT_BRACE",
+}
+
+class Scanner:
+    def __init__(self, src):
+        self.src = src
+        self.tkns = []
+        self.strt = 0
+        self.curr = 0
+    
+    def is_at_end(self):
+        return self.curr >= len(self.src)
+
 
 def main():
 
-    if len(sys.argv) < 3:
-        print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
-        exit(1)
+    # filename = sys.argv[1]
 
-    command = sys.argv[1]
-    filename = sys.argv[2]
+    # with open(filename) as file:
+    #     file_contnts = file.read()
 
-    if command != "tokenize":
-        print(f"Unknown command: {command}", file=sys.stderr)
-        exit(1)
+    scanner = Scanner(")(}{)")
+    all_tkns_symbls = all_tkns.keys()
+    for char in scanner.src:
+        if char in all_tkns_symbls:
+            scanner.tkns.append(char)
+        print(f"{all_tkns[char]} {char} null")
 
-    with open(filename) as file:
-        file_contents = file.read()
-
-    for char in file_contents:
-        if char == '(':
-            print(f"LEFT_PAREN {char} null")
-        elif char == ')':
-            print(f"RIGHT_PAREN {char} null")
-        elif char == '{':
-            print(f"LEFT_BRACE {char} null")
-        elif char == '}':
-            print(f"RIGHT_BRACE {char} null")
-        elif char == ',':
-            print(f"COMMA {char} null")
-        elif char == '.':
-            print(f"DOT {char} null")
-        elif char == '-':
-            print(f"MINUS {char} null")
-        elif char == '+':
-            print(f"PLUS {char} null")
-        elif char == ';':
-            print(f"SEMICOLON {char} null")
-        elif char == '*':
-            print(f"STAR {char} null")
-    print("EOF  null")
-
+    
 if __name__ == "__main__":
     main()
